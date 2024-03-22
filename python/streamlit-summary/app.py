4cc2e5d68dd22768b10e1b179e757c2b0efd4175
Kirk Marple <kirk@unstruk.com>
2024-03-21 22:00:13 -0700

    .

--- python/streamlit-summary/app.py
+++ python/streamlit-summary/app.py
@@ -243,7 +243,7 @@ if submit_summary:
     
 with st.sidebar:
     with st.form("credentials_form"):
-        st.info("Locate connection information for project in [Graphlit Developer Portal](https://portal.graphlit.dev/)")
+        st.info("Locate connection information for your project in the [Graphlit Developer Portal](https://portal.graphlit.dev/)")
         st.session_state['secret_key'] = st.text_input("Secret Key", type="password")
         st.session_state['environment_id'] = st.text_input("Environment ID")
         st.session_state['organization_id'] = st.text_input("Organization ID")
