f61b90b39d99647ba4ba306ee50955c078133f57
Kirk Marple <kirk@unstruk.com>
2024-03-21 22:28:19 -0700

    .

--- python/streamlit-chat/app.py
+++ python/streamlit-chat/app.py
@@ -92,10 +92,12 @@ with st.sidebar:
         ### Demo Instructions
         - **Step 1:** Generate Graphlit project token.
         - **Step 2:** Enter a prompt to ask about your content.
+        """)
 
-        - [Sign up for Graphlit](https://docs.graphlit.dev/getting-started/signup)            
-        - [API Reference](https://docs.graphlit.dev/graphlit-data-api/api-reference)     
-        - [More information](https://www.graphlit.com)      
+    st.markdown("""
+        [Sign up for Graphlit](https://docs.graphlit.dev/getting-started/signup)            
+        [API Reference](https://docs.graphlit.dev/graphlit-data-api/api-reference)     
+        [More information](https://www.graphlit.com)      
         """)
 
     with st.form("credentials_form"):
