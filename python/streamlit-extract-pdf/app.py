543fcf01b06f72d5c342f9da020fc8f4e405b89a
Kirk Marple <kirk@unstruk.com>
2024-03-25 18:49:59 -0700

    .

--- python/streamlit-extract-pdf/app.py
+++ python/streamlit-extract-pdf/app.py
@@ -15,10 +15,6 @@ if 'workflow_id' not in st.session_state:
     st.session_state['workflow_id'] = None
 if 'content_id' not in st.session_state:
     st.session_state['content_id'] = None
-if 'specification_id' not in st.session_state:
-    st.session_state['specification_id'] = None
-if 'conversation_id' not in st.session_state:
-    st.session_state['conversation_id'] = None
 if 'environment_id' not in st.session_state:
     st.session_state['environment_id'] = ""
 if 'organization_id' not in st.session_state:
@@ -31,8 +27,10 @@ if 'document_markdown' not in st.session_state:
     st.session_state['document_markdown'] = None
 if 'document_metadata' not in st.session_state:
     st.session_state['document_metadata'] = None
+if 'document_observations' not in st.session_state:
+    st.session_state['document_observations'] = None
 
-def get_content_metadata():
+def get_content():
     # Define the GraphQL mutation
     query = """
     query GetContent($id: ID!) {
@@ -44,6 +42,16 @@ def get_content_metadata():
                 title
                 keywords
                 author
+            }
+            observations {
+                type
+                occurrences {
+                    type
+                    pageIndex
+                }
+                observable {
+                    name
+                }
             }            
         }
     }
@@ -59,7 +67,7 @@ def get_content_metadata():
  #   st.json(response)
 
     if 'content' in response['data']:
-        return response['data']['content']['document'], response['data']['content']['markdown']
+        return response['data']['content']['document'], response['data']['content']['markdown'], response['data']['content']['observations']
     
     return None
 
@@ -160,19 +168,17 @@ def create_workflow():
     # Define the variables for the mutation
     variables = {
         "workflow": {
-            "preparation": {
+            "extraction": {
                 "jobs": [
                     {
                     "connector": {
-                        "type": "AZURE_DOCUMENT_INTELLIGENCE",
-                        "azureDocument": {
-                            "model": "LAYOUT"
-                        }
+                        "type": "AZURE_COGNITIVE_SERVICES_TEXT",
+                        "extractedTypes": [ "ORGANIZATION", "PERSON" ]
                     }
                     }
                 ]
             },
-            "name": "Azure AI Document Intelligence"
+            "name": "Entity Extraction"
         }
     }
 
@@ -187,151 +193,16 @@ def create_workflow():
 
     return None
 
-def delete_specification():
-    # Define the GraphQL mutation
-    query = """
-    mutation DeleteSpecification($id: ID!) {
-        deleteSpecification(id: $id) {
-            id
-        }
-        }
-    """
-
-    # Define the variables for the mutation
-    variables = {
-        "id": st.session_state['specification_id']
-    }
-    response = st.session_state['client'].request(query=query, variables=variables)
-
-def create_specification():
-    # Define the GraphQL mutation
-    mutation = """
-    mutation CreateSpecification($specification: SpecificationInput!) {
-        createSpecification(specification: $specification) {
-            id
-        }
-    }
-    """
-
-    # Define the variables for the mutation
-    variables = {
-        "specification": {
-            "type": "COMPLETION",
-            "serviceType": "ANTHROPIC",
-            "anthropic": {
-                "model": "CLAUDE_3_HAIKU",
-                "temperature": 0.1,
-                "probability": 0.2,
-                "completionTokenLimit": 2048
-            },
-            "strategy": { 
-                "enableExpandedRetrieval": True # enable small-to-big retrieval
-            },
-            "promptStrategy": { 
-                "type": "OPTIMIZE_SEARCH" # rewrite prompt to optimize for semantic search
-            },
-            "name": "Completion"
-        }
-    }
-
-    # Convert the request to JSON format
-    response = st.session_state['client'].request(query=mutation, variables=variables)
-
-    if 'errors' in response and len(response['errors']) > 0:
-        error_message = response['errors'][0]['message']
-        return error_message
-
-    st.session_state['specification_id'] = response['data']['createSpecification']['id']
-
-    return None
-
-def delete_conversation():
-    # Define the GraphQL mutation
-    query = """
-    mutation DeleteConversation($id: ID!) {
-        deleteConversation(id: $id) {
-            id
-        }
-        }
-    """
-
-    # Define the variables for the mutation
-    variables = {
-        "id": st.session_state['conversation_id']
-    }
-    response = st.session_state['client'].request(query=query, variables=variables)
-
-def create_conversation():
-    # Define the GraphQL mutation
-    mutation = """
-    mutation CreateConversation($conversation: ConversationInput!) {
-    createConversation(conversation: $conversation) {
-        id
-    }
-    }
-    """
-
-    variables = {
-        "conversation": {
-            "specification": {
-                "id": st.session_state['specification_id']
-            },
-            "name": "Conversation"
-        }
-    }
-
-    # Send the GraphQL request with the JWT token in the headers
-    response = st.session_state['client'].request(query=mutation, variables=variables)
-
-    if 'errors' in response and len(response['errors']) > 0:
-        error_message = response['errors'][0]['message']
-        return error_message
-
-    st.session_state['conversation_id'] = response['data']['createConversation']['id']
-
-    return None
-
-def prompt_conversation(prompt):
-    # Define the GraphQL mutation
-    mutation = """
-    mutation PromptConversation($prompt: String!, $id: ID) {
-    promptConversation(prompt: $prompt, id: $id) {
-        message {
-            message
-        }
-    }
-    }
-    """
-
-    # Define the variables for the mutation
-    variables = {
-        "prompt": prompt,
-        "id": st.session_state['conversation_id']
-    }
-
-    # Send the GraphQL request with the JWT token in the headers
-    response = st.session_state['client'].request(query=mutation, variables=variables)
-
-    if 'errors' in response and len(response['errors']) > 0:
-        error_message = response['errors'][0]['message']
-        return None, error_message
-
-    message = response['data']['promptConversation']['message']['message']
-
-    return message, None
-       
 st.image("https://graphlitplatform.blob.core.windows.net/samples/graphlit-logo.svg", width=128)
 st.title("Graphlit Platform")
-st.markdown("Chat with any PDF, DOCX, or PPTX file.  Text extraction and OCR done with [Azure AI Document Intelligence](https://azure.microsoft.com/en-us/products/ai-services/ai-document-intelligence).")
+st.markdown("Extract people and companies from any PDF, DOCX, or PPTX file.")
 
 if st.session_state['token'] is None:
     st.info("To get started, generate a token to connect to your Graphlit project.")
 
 # A dictionary mapping PDF names to their PDF URIs
 pdfs = {
-    "Attention is all you need": "https://graphlitplatform.blob.core.windows.net/samples/Attention%20Is%20All%20You%20Need.1706.03762.pdf",
     "Uber 10Q (March 2022)": "https://graphlitplatform.blob.core.windows.net/samples/uber_10q_march_2022.pdf",
-    "Unifying Large Language Models and Knowledge Graphs: A Roadmap": "https://graphlitplatform.blob.core.windows.net/samples/Unifying%20Large%20Language%20Models%20and%20Knowledge%20Graphs%20A%20Roadmap-2306.08302.pdf",
 }
 
 document_metadata = None
@@ -398,10 +269,11 @@ with st.form("data_content_form"):
 
                 st.success(f"Document ingestion took {duration:.2f} seconds. Finished at {formatted_time} UTC.")
 
-                document_metadata, document_markdown = get_content_metadata()
+                document_metadata, document_markdown, document_observations = get_content()
 
                 st.session_state['document_metadata'] = document_metadata
                 st.session_state['document_markdown'] = document_markdown
+                st.session_state['document_observations'] = document_observations
 
                 placeholder = st.empty()
         else:
@@ -413,6 +285,7 @@ if st.session_state['content_done'] == True:
 
         document_metadata = st.session_state['document_metadata']
         document_markdown = st.session_state['document_markdown']
+        document_observations = st.session_state['document_observations']
 
         if document_metadata is not None:
             document_title = document_metadata["title"]
@@ -427,50 +300,19 @@ if st.session_state['content_done'] == True:
         if document_markdown is not None:
             with st.expander("See extracted document text:", expanded=False):
                 st.markdown(document_markdown)
+        
+        if document_observations is not None:
+            st.header('Extracted people and organizations:')
 
-        if "messages" not in st.session_state:
-            st.session_state.messages = []
-
-        for message in st.session_state.messages:
-            with st.chat_message(message["role"]):
-                st.markdown(message["content"])
-
-        if st.session_state['specification_id'] is None:
-            error_message = create_specification()
-
-            if error_message is not None:
-                st.error(f"Failed to create specification. {error_message}")
-
-        if st.session_state['conversation_id'] is None:
-            error_message = create_conversation()
-
-            if error_message is not None:
-                st.error(f"Failed to create conversation. {error_message}")
-
-        try:
-            if prompt := st.chat_input("Ask me anything about this document."):
-                st.session_state.messages.append({"role": "user", "content": prompt})
-                
-                with st.chat_message("user"):
-                    st.markdown(prompt)
-                
-                with st.chat_message("assistant"):
-                    response, error_message = prompt_conversation(prompt)
-                    
-                    if error_message is not None:
-                        st.error(f"Failed to prompt specification. {error_message}")
-                    else:
-                        st.markdown(response)
-                        st.session_state.messages.append({"role": "assistant", "content": response})
-        except:
-            st.warning("You need to generate a token before chatting with your document.")
+            # TODO: pretty render the observations
+            st.json(document_observations)
 
 with st.sidebar:
     st.info("""
         ### Demo Instructions
         - **Step 1:** Generate Graphlit project token.
         - **Step 2:** Select a PDF, or fill in your own document URL.
-        - **Step 3:** Enter a prompt to ask about the document using [Anthropic](https://www.anthropic.com) Claude 3 Haiku LLM.
+        - **Step 3:** TODO
         """)
 
     with st.form("credentials_form"):
