5fe108e70c5da218d7a0608c66e043c6128a721e
Kirk Marple <kirk@unstruk.com>
2024-03-31 11:53:09 -0700

    .

--- python/streamlit-publish-github-issues/app.py
+++ python/streamlit-publish-github-issues/app.py
@@ -241,7 +241,7 @@ websites = {
 }
     
 with st.form("data_feed_form"):
-    personal_access_token = st.text_input("Enter your GitHub personal access token (PAT):")
+    personal_access_token = st.text_input("Enter your [GitHub personal access token](https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/managing-your-personal-access-tokens):")
 
     selected_website = st.selectbox("Select a GitHub repo:", options=list(websites.keys()))
     
