cd929ac156988248741a477afc95e019c4c576e1
Kirk Marple <kirk@unstruk.com>
2024-04-05 22:18:59 -0700

    .

--- python/streamlit-summary-website/app.py
+++ python/streamlit-summary-website/app.py
@@ -211,7 +211,7 @@ def generate_summary():
 
 st.image("https://graphlitplatform.blob.core.windows.net/samples/graphlit-logo.svg", width=128)
 st.title("Graphlit Platform")
-st.markdown("Generate summary of website. Will scrape website, and read a maximum of 10 pages via web sitemap.")
+st.markdown("Generate summary of website. Will scrape website, and read a maximum of 10 pages via web sitemap. Summary generated with the [Anthropic](https://www.anthropic.com) Claude 3 Haiku LLM.")
 
 if st.session_state['token'] is None:
     st.info("💡 To get started, generate a token in the side panel to connect to your Graphlit project.")
