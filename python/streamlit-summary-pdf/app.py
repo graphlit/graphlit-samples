e69a02f40d4c157833960cf1c37c52bf8084052a
Kirk Marple <kirk@unstruk.com>
2024-04-07 20:15:37 -0700

    .

--- python/streamlit-summary-pdf/app.py
+++ python/streamlit-summary-pdf/app.py
@@ -7,30 +7,26 @@ import time
 from graphlit_client import Graphlit
 
 # Initialize session state variables if not already done
-if 'client' not in st.session_state:
-    st.session_state['client'] = None
 if 'token' not in st.session_state:
     st.session_state['token'] = None
+if 'uri' not in st.session_state:
+    st.session_state['uri'] = None
+if 'specification_id' not in st.session_state:
+    st.session_state['specification_id'] = None
 if 'workflow_id' not in st.session_state:
     st.session_state['workflow_id'] = None
 if 'content_id' not in st.session_state:
     st.session_state['content_id'] = None
 if 'content_done' not in st.session_state:
     st.session_state['content_done'] = None    
-if 'specification_id' not in st.session_state:
-    st.session_state['specification_id'] = None
-if 'conversation_id' not in st.session_state:
-    st.session_state['conversation_id'] = None
+if 'client' not in st.session_state:
+    st.session_state['client'] = None
 if 'environment_id' not in st.session_state:
     st.session_state['environment_id'] = ""
 if 'organization_id' not in st.session_state:
     st.session_state['organization_id'] = ""
 if 'secret_key' not in st.session_state:
     st.session_state['secret_key'] = ""
-if 'document_markdown' not in st.session_state:
-    st.session_state['document_markdown'] = None
-if 'document_metadata' not in st.session_state:
-    st.session_state['document_metadata'] = None
 
 def ingest_file(uri):
     # Define the GraphQL mutation
@@ -38,13 +34,6 @@ def ingest_file(uri):
     mutation IngestUri($uri: URL!, $workflow: EntityReferenceInput, $isSynchronous: Boolean) {
         ingestUri(uri: $uri, workflow: $workflow, isSynchronous: $isSynchronous) {
             id
-            markdown
-            document {
-                title
-                keywords
-                author
-                pageCount
-            }
         }
     }
     """
@@ -63,11 +52,11 @@ def ingest_file(uri):
 
     if 'errors' in response and len(response['errors']) > 0:
         error_message = response['errors'][0]['message']
-        return None, None, error_message
+        return error_message
 
     st.session_state['content_id'] = response['data']['ingestUri']['id']
 
-    return response['data']['ingestUri']['document'], response['data']['ingestUri']['markdown'], None
+    return None
 
 def delete_content():
     # Define the GraphQL mutation
@@ -164,6 +153,10 @@ def create_specification():
     mutation CreateSpecification($specification: SpecificationInput!) {
         createSpecification(specification: $specification) {
             id
+            name
+            state
+            type
+            serviceType
         }
     }
     """
@@ -172,21 +165,14 @@ def create_specification():
     variables = {
         "specification": {
             "type": "COMPLETION",
-            "serviceType": "OPEN_AI",
-            "openAI": {
-                "model": "GPT4_TURBO_128K",
-                "temperature": 0.1,
+            "serviceType": "ANTHROPIC",
+            "anthropic": {
+                "model": "CLAUDE_3_HAIKU",
+                "temperature": 0.5,
                 "probability": 0.2,
                 "completionTokenLimit": 2048
             },
-            "promptStrategy": { 
-                "type": "OPTIMIZE_SEARCH" # rewrite prompt to optimize for semantic search
-            },
-            "retrievalStrategy": {
-                "type": "SECTION",
-                "contentLimit": 10,
-            },
-            "name": "Completion"
+            "name": "Summarization"
         }
     }
 
@@ -207,6 +193,7 @@ def delete_specification():
     mutation DeleteSpecification($id: ID!) {
         deleteSpecification(id: $id) {
             id
+            state
         }
         }
     """
@@ -217,91 +204,55 @@ def delete_specification():
     }
     response = st.session_state['client'].request(query=query, variables=variables)
 
-def create_conversation():
+def generate_summary():
     # Define the GraphQL mutation
     mutation = """
-    mutation CreateConversation($conversation: ConversationInput!) {
-    createConversation(conversation: $conversation) {
-        id
+    mutation SummarizeContents($summarizations: [SummarizationStrategyInput!]!, $filter: ContentFilter) {
+    summarizeContents(summarizations: $summarizations, filter: $filter) {
+        items {
+            text
+        }
     }
     }
     """
 
+    # Define the variables for the mutation
     variables = {
-        "conversation": {
+    "filter": {
+        "types": [
+            "FILE"
+        ],
+        "contents": [
+            { 
+                "id": st.session_state["content_id"]
+            }
+        ]
+    },
+    "summarizations": [
+        {
+            "type": "SUMMARY",
             "specification": {
-                "id": st.session_state['specification_id']
-            },
-            "filter": {
-                "contents":[
-                    {
-                        "id": st.session_state['content_id']                        
-                    }
-                ]
-            },
-            "name": "Conversation"
+                "id": st.session_state["specification_id"]
+            }
         }
+    ]
     }
 
-    # Send the GraphQL request with the JWT token in the headers
     response = st.session_state['client'].request(query=mutation, variables=variables)
 
     if 'errors' in response and len(response['errors']) > 0:
         error_message = response['errors'][0]['message']
-        return error_message
+        st.error(error_message)
+        return "No summary was generated."
 
-    st.session_state['conversation_id'] = response['data']['createConversation']['id']
+    if 'summarizeContents' in response['data'] and len(response['data']['summarizeContents']) > 0:
+        return "\n\n".join(item['text'] for content in response['data']['summarizeContents'] for item in content['items'])
+    
+    return "No summary was generated."
 
-    return None
-
-def delete_conversation():
-    # Define the GraphQL mutation
-    query = """
-    mutation DeleteConversation($id: ID!) {
-        deleteConversation(id: $id) {
-            id
-        }
-        }
-    """
-
-    # Define the variables for the mutation
-    variables = {
-        "id": st.session_state['conversation_id']
-    }
-    response = st.session_state['client'].request(query=query, variables=variables)
-
-def prompt_conversation(prompt):
-    # Define the GraphQL mutation
-    mutation = """
-    mutation PromptConversation($prompt: String!, $id: ID) {
-    promptConversation(prompt: $prompt, id: $id) {
-        message {
-            message
-        }
-    }
-    }
-    """
-
-    # Define the variables for the mutation
-    variables = {
-        "prompt": prompt,
-        "id": st.session_state['conversation_id']
-    }
-
-    # Send the GraphQL request with the JWT token in the headers
-    response = st.session_state['client'].request(query=mutation, variables=variables)
-
-    if 'errors' in response and len(response['errors']) > 0:
-        error_message = response['errors'][0]['message']
-        return None, error_message
-
-    message = response['data']['promptConversation']['message']['message']
-
-    return message, None
-       
 st.image("https://graphlitplatform.blob.core.windows.net/samples/graphlit-logo.svg", width=128)
 st.title("Graphlit Platform")
-st.markdown("Chat with any PDF, DOCX, or PPTX file. Text extraction and OCR done with [Azure AI Document Intelligence](https://azure.microsoft.com/en-us/products/ai-services/ai-document-intelligence).  Chat completion uses the [OpenAI GPT-4 Turbo 128k](https://platform.openai.com/docs/models/gpt-4-and-gpt-4-turbo) LLM.")
+st.markdown("Summarize any PDF, DOCX, or PPTX file. Summary generated with the [Anthropic](https://www.anthropic.com) Claude 3 Haiku LLM.")
 
 if st.session_state['token'] is None:
     st.info("💡 To get started, generate a token in the side panel to connect to your Graphlit project.")
@@ -310,13 +261,12 @@ if st.session_state['token'] is None:
 pdfs = {
     "Attention is all you need": "https://graphlitplatform.blob.core.windows.net/samples/Attention%20Is%20All%20You%20Need.1706.03762.pdf",
     "Unifying Large Language Models and Knowledge Graphs: A Roadmap": "https://graphlitplatform.blob.core.windows.net/samples/Unifying%20Large%20Language%20Models%20and%20Knowledge%20Graphs%20A%20Roadmap-2306.08302.pdf",
-    "Microsoft 10Q (March 2024)": "https://graphlitplatform.blob.core.windows.net/samples/MSFT_FY24Q1_10Q.docx",
-    "Uber 10Q (March 2022)": "https://graphlitplatform.blob.core.windows.net/samples/uber_10q_march_2022.pdf",
+    "On Approximate Nearest Neighbour Selection for Multi-Stage Dense Retrieval": "https://arxiv.org/pdf/2108.11480.pdf"
 }
 
 document_metadata = None
 document_markdown = None
-
+    
 with st.form("data_content_form"):
     selected_pdf = st.selectbox("Select a PDF:", options=list(pdfs.keys()))
     
@@ -328,9 +278,6 @@ with st.form("data_content_form"):
 
     # Now, handle actions based on submit_data outside the form's scope
     if submit_content and uri:
-        st.session_state.messages = []
-        st.session_state['content_done'] = False
-
         if st.session_state['token']:
             st.session_state['uri'] = uri
             
@@ -350,14 +297,11 @@ with st.form("data_content_form"):
 
             # Display spinner while processing
             with st.spinner('Ingesting document... Please wait.'):
-                document_metadata, document_markdown, error_message = ingest_file(uri)
+                error_message = ingest_file(uri)
 
                 if error_message is not None:
                     st.error(f"Failed to ingest file [{uri}]. {error_message}")
                 else:
-                    st.session_state['document_metadata'] = document_metadata
-                    st.session_state['document_markdown'] = document_markdown
-
                     # Once done, notify the user
                     st.session_state["content_done"] = True
 
@@ -372,6 +316,31 @@ with st.form("data_content_form"):
         else:
             st.error("Please fill in all the connection information.")
 
+if st.session_state['content_done'] == True:
+    if st.session_state['token']:
+        if st.session_state['specification_id'] is not None:
+            with st.spinner('Deleting existing specification... Please wait.'):
+                delete_specification()
+            st.session_state["specification_id"] = None
+
+        error_message = create_specification()
+
+        if error_message is not None:
+            st.error(error_message)
+        else:
+            start_summary_time = time.time()
+
+            with st.spinner('Generating summary... Please wait.'):
+                summary = generate_summary()
+                placeholder.markdown(summary)
+
+                summary_duration = time.time() - start_summary_time
+
+                current_time = datetime.now()
+                formatted_time = current_time.strftime("%H:%M:%S")
+
+                st.success(f"Summary generation took {summary_duration:.2f} seconds. Finished at {formatted_time} UTC.")
+
 with st.form("clear_data_form"):
     st.markdown("If you run into any problems, or exceeded your Free Tier project quota, you can delete all your contents to start over.  Be aware, this deletes *all* the contents in your project.")
 
@@ -382,84 +351,13 @@ with st.form("clear_data_form"):
             with st.spinner('Deleting contents... Please wait.'):
                 delete_all_contents()
 
-if st.session_state['content_done'] == True:
-    if st.session_state['token']:
-        st.markdown(f"**Document URI:** {uri}")
-
-        document_metadata = st.session_state['document_metadata']
-        document_markdown = st.session_state['document_markdown']
-
-        if document_metadata is not None:
-            document_title = None
-            document_author = None
-            document_pageCount = None
-
-            if 'title' in document_metadata:
-                document_title = document_metadata["title"]
-
-            if 'author' in document_metadata:
-                document_author = document_metadata["author"]
-
-            if 'pageCount' in document_metadata:
-                document_pageCount = document_metadata["pageCount"]
-
-            if document_title is not None:
-                st.markdown(f"**Title:** {document_title}")
-
-            if document_author is not None:
-                st.markdown(f"**Author:** {document_author}")
-
-            if document_pageCount is not None:
-                st.markdown(f"**Page count:** {document_pageCount}")
-
-        if document_markdown is not None:
-            with st.expander("See document text:", expanded=False):
-                st.markdown(document_markdown)
-
-        if "messages" not in st.session_state:
-            st.session_state.messages = []
-
-        for message in st.session_state.messages:
-            with st.chat_message(message["role"]):
-                st.markdown(message["content"])
-
-        if st.session_state['specification_id'] is None:
-            error_message = create_specification()
-
-            if error_message is not None:
-                st.error(f"Failed to create specification. {error_message}")
-
-        if st.session_state['conversation_id'] is None:
-            error_message = create_conversation()
-
-            if error_message is not None:
-                st.error(f"Failed to create conversation. {error_message}")
-
-        try:
-            if prompt := st.chat_input("Ask me anything about this document."):
-                st.session_state.messages.append({"role": "user", "content": prompt})
-                
-                with st.chat_message("user"):
-                    st.markdown(prompt)
-                
-                with st.chat_message("assistant"):
-                    response, error_message = prompt_conversation(prompt)
-                    
-                    if error_message is not None:
-                        st.error(f"Failed to prompt conversation. {error_message}")
-                    else:
-                        st.markdown(response)
-                        st.session_state.messages.append({"role": "assistant", "content": response})
-        except:
-            st.warning("You need to generate a token before chatting with your document.")
-
 with st.sidebar:
     st.info("""
         ### Demo Instructions
         - [Sign up for Graphlit](https://docs.graphlit.dev/getting-started/signup) 🆓  
         - **Step 1:** Generate Graphlit project token.
         - **Step 2:** Select a PDF, or fill in your own document URL.
-        - **Step 3:** Enter a prompt to ask about the document using [OpenAI GPT-4 Turbo 128k](https://platform.openai.com/docs/models/gpt-4-and-gpt-4-turbo) LLM.
+        - **Step 3:** Click to generate summary of file using [Anthropic](https://www.anthropic.com) Claude 3 Haiku LLM.     
         """)
 
     with st.form("credentials_form"):
